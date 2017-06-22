73aaf6001f369f336541b4463b007e6076dfffae
Инна Грант <grant@SMS.SMS>
2017-06-22 17:10:19 +0300

    Added old jQuery back

--- js/jquery.equalHeights.js
+++ js/jquery.equalHeights.js
@@ -1,10 +1,13 @@
-$.fn.equalHeights = function (el) {
-    $(el).each(function () {
-        var currentTallest = 0;
-        $(el).each(function (i) {
-            if ($(this).height() > currentTallest) { currentTallest = $(this).height(); }
+$(function() {
+    $.fn.equalHeights = function (el) {
+        $(el).each(function () {
+            var currentTallest = 0;
+            $(el).each(function (i) {
+                if ($(this).height() > currentTallest) { currentTallest = $(this).height(); }
+            });
+            $(el).css({ 'min-height': currentTallest });
         });
-        $(el).css({ 'min-height': currentTallest });
-    });
-    return this;
-};
+        return this;
+    };
+
+});
